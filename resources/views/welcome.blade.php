30c1ab1e95e6f2a877eacf6be0b5b1ff748669a5
hafiz2912 <hafiz.240170162@mhs.unimal.ac.id>
2026-05-03 17:16:49 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chirper - Tugas</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 antialiased">
+
+    <nav class="p-6 flex justify-between items-center max-w-7xl mx-auto">
+        <div class="flex items-center gap-2 font-bold text-xl text-gray-800">
+            <span class="text-red-500">🐦</span> Chirper
+        </div>
+        <div class="space-x-4">
+            <a href="/login" class="text-gray-600 font-semibold hover:underline">Sign In</a>
+            <a href="/register" class="bg-black text-white px-4 py-2 rounded-lg font-semibold shadow-md">Sign Up</a>
+        </div>
+    </nav>
+
+    <div>
+    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+    <p>This is your brand new Laravel app. Time to make it sing (or chirp)!</p>
+    <p class="mt-2 text-sm text-gray-600">Now this is live on the internet! 🎉🚀</p>
+    </div>
+
+    <footer class="fixed bottom-10 left-0 right-0 flex justify-center">
+        <div class="border-2 border-red-600 px-6 py-2 rounded-full text-sm font-bold text-gray-700 bg-white shadow-sm">
+            © 2026 Chirper - Built with Laravel and ❤️ by Afni Zahara (240170046)
+        </div>
+    </footer>
+
+</body>
+</html>
